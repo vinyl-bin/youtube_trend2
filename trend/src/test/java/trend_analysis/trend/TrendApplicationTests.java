e96264d390aa27455ffd664375f2017d8ae27edc
dabin <db021129@naver.com>
2024-08-04 16:13:35 +0900

    feat: Add Project Frame

---
+++ trend/src/test/java/trend_analysis/trend/TrendApplicationTests.java
@@ -0,0 +1,13 @@
+package trend_analysis.trend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
